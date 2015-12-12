80666acea6cf8b3ec5179b36b6ec4983818045a9
MrBlaise <rostas.balazs@gmail.com>
2015-12-12 12:57:11 +0100

    Overkill but cool

--- Day1/solution.py
+++ Day1/solution.py
@@ -1,11 +1,12 @@
-f = open("input.txt", "rt")
-floor =0
+with open("input.txt", "rt") as f:
+    floor = sum([1 if char == "(" else -1 for line in f for char in line])
+print floor
 
+"""
 for line in f:
     for char in line:
         if char == '(':
             floor += 1
         else:
             floor -= 1
-
-print floor
+"""
